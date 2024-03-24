5a940ba32a5ffb8c80a74bc0ebbe57a6fface0fd
libmartinito <lib.martinito@pm.me>
2024-03-24 16:53:06 +0800

    pass replication 1st stage

--- app/server.go
+++ app/server.go
@@ -1,16 +1,33 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net"
 	"os"
 )
 
 func main() {
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
-	if err != nil {
-		fmt.Println("Failed to bind to port 6379")
-		os.Exit(1)
+	port := flag.String("port", "6379", "port to listen on")
+
+	flag.Parse()
+
+	var l net.Listener
+	var err error
+
+	switch {
+	case *port != "6379":
+		l, err = net.Listen("tcp", "0.0.0.0:"+*port)
+		if err != nil {
+			fmt.Println("Failed to bind to port " + *port)
+			os.Exit(1)
+		}
+	default:
+		l, err = net.Listen("tcp", "0.0.0.0:6379")
+		if err != nil {
+			fmt.Println("Failed to bind to port 6379")
+			os.Exit(1)
+		}
 	}
 
 	for {
