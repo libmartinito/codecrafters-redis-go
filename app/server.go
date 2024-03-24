24db300007291a8227c34006114de2ee11f664c1
libmartinito <lib.martinito@pm.me>
2024-03-24 10:12:59 +0800

    pass 2nd stage

--- app/server.go
+++ app/server.go
@@ -4,22 +4,57 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
-func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
+func parseResp(b []byte) (cmd string, args []string) {
+	respElementsArr := strings.Split(string(b), "\r\n")
 
-	// Uncomment this block to pass the first stage
-	//
+	for _, v := range respElementsArr {
+		if v == "" {
+			continue
+		}
+
+		if v[0] == '*' {
+			continue
+		}
+
+		if v[0] == '$' {
+			continue
+		}
+
+		if cmd == "" {
+			cmd = v
+		} else {
+			args = append(args, v)
+		}
+	}
+
+	return cmd, args
+}
+
+func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+
+	c, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
+	}
+
+	b := make([]byte, 1024)
+	_, err = c.Read(b)
+	if err != nil {
+		fmt.Println("Error reading from connection: ", err.Error())
+		os.Exit(1)
+	}
+
+	cmd, _ := parseResp(b)
+	if cmd == "ping" {
+		c.Write([]byte("+PONG\r\n"))
 	}
 }
